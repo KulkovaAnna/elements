38bdfb8091b4817e227e750d60149e7e17dd52f8
Anna Kulkova <anna.kulkova@interexy.com>
2022-06-30 11:20:59 +0300

    feat: update urls

--- next.config.js
+++ next.config.js
@@ -7,35 +7,30 @@ module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
   const isProd =
     phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
-  const isStaging =
-    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1';
 
-  console.log(`isDev:${isDev}  isProd:${isProd}   isStaging:${isStaging}`);
+  console.log(`isDev:${isDev}  isProd:${isProd}`);
 
   const env = {
     MEDIA_HOST: (() => {
       if (isDev) return '192.168.100.161';
       if (isProd) {
-        return '192.168.100.161';
+        return 'ipoe-api.herokuapp.com';
       }
-      if (isStaging) return '192.168.100.161';
-      return 'MEDIA_HOST:not (isDev,isProd && !isStaging,isProd && isStaging)';
+      return 'MEDIA_HOST:not (isDev,isProd && !isStaging,isProd)';
     })(),
     GRAPHQL_API_URL: (() => {
       if (isDev) return 'http://192.168.100.161:5000/graphql';
       if (isProd) {
-        return 'http://192.168.100.161:5000/graphql';
+        return 'https://ipoe-api.herokuapp.com/graphql';
       }
-      if (isStaging) return 'http://192.168.100.161:5000/graphql';
-      return 'GRAPHQL_API_HOST:not (isDev,isProd && !isStaging,isProd && isStaging)';
+      return 'GRAPHQL_API_HOST:not (isDev,isProd && !isStaging,isProd)';
     })(),
     STORAGE_URL: (() => {
       if (isDev) return 'http://192.168.100.161:5000';
       if (isProd) {
-        return 'http://192.168.100.161:5000';
+        return 'https://ipoe-api.herokuapp.com/graphql';
       }
-      if (isStaging) return 'http://192.168.100.161:5000';
-      return 'STORAGE_URL:not (isDev,isProd && !isStaging,isProd && isStaging)';
+      return 'STORAGE_URL:not (isDev,isProd && !isStaging,isProd)';
     })(),
   };
 
